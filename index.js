d34729f4129e79312194be0873235e0ec76476bd
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-07-12 14:02:52 -0500

    done

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const puppeteer = require('puppeteer');
 const cheerio  = require('cheerio');
+const mongoose = require('mongoose');
+const Listings = require("./model/Listings");
 
-async function main() {
-  const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage();
+async function connecttoMongoDb() {
+  await mongoose.connect(`mongodb+srv://test:test@craiglistjobs.zcpkv.mongodb.net/<craiglistListings>?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('connected to mongodb');
+}
+
+async function scrapeListings(page) {
   await page.goto("https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof");
 
   const html = await page.content();
   const $ = cheerio.load(html);
-
-  const results = $(".result-info").map((index, element) => {
+  const listings = $(".result-info").map((index, element) => {
     const titleElement = $(element).find(".result-title");
     const timeElement = $(element).find(".result-date")
     const hoodElement = $(element).find(".result-hood");
     const title = $(titleElement).text();
     const url = $(titleElement).attr('href');
-    const dataPosted = new Date($(timeElement).attr('datetime'));
-    const hood = $(hoodElement)
+    const datePosted = new Date($(timeElement).attr('datetime'));
+    const neighborhood = $(hoodElement)
       .text()
       .trim()
       .replace("(", "")
@@ -25,11 +29,42 @@ async function main() {
     return {
       title,
       url,
-      dataPosted,
-      hood
+      datePosted,
+      neighborhood
     }
   }).get();
-  console.log(results);
+  return listings;
+}
+
+async function scrapeJobDescriptions(listings, page) {
+  for (var i = 0; i < listings.length; i++) {
+    await page.goto(listings[i].url);
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const jobDescription = $("#postingbody").text(); 
+    const compensation = $(".attrgroup > span:nth-child(1) > b").text();
+    listings[i].jobDescription = jobDescription;
+    listings[i].compensation = compensation;
+    console.log(listings[i].jobDescription);
+    console.log(listings[i].compensation);
+    const listingsModel = new Listings(listings[i]);
+    await listingsModel.save();
+    await sleep(1000); // 1 second sleep
+  }
+}
+
+async function sleep(miliseconds) {
+  return new Promise(resolve => setTimeout(resolve, miliseconds));
+}
+
+async function main() {
+  await connecttoMongoDb();
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage();
+  const listings = await scrapeListings(page);
+  const listingsWithjobDescription = await scrapeJobDescriptions(listings, page);
+  console.log(listings);
+
 }
 
 main();
